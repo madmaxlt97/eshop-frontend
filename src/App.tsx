8b2f2047affeafc72d5a30e1eca7347f887601cd
Max <madmaxlt97@gmail.com>
2025-04-29 22:05:37 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+const API_HOST = import.meta.env.VITE_API_HOST;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("Dar nieko");
+
+  useEffect(() => {
+    axios
+      .get(API_HOST)
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((err) => alert(err));
+  });
 
   return (
     <>
       <div>
+        <h1>{message}</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
